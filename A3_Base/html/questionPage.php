23e7671ce9b26d49a2ba0ace4b1182ab2a6bebd3
Henrique Freitas <up201707046@fe.up.pt>
2020-03-14 14:41:19 -0100

    question page shit

--- A3_Base/html/questionPage.php
+++ A3_Base/html/questionPage.php
@@ -13,84 +13,83 @@ drawHTMlHeader();
     ?>
 
 
-    <main id="main" class="ml-lg-auto col-lg-10 text-center h-100 pr-5">
-        <div class="container-fluid py-2 my-3 pr-lg-5 text-left bg-white ">
-            <div class="blockquote py-2 pr-lg-5">
+    <main id="main" class="ml-auto col-lg-10 text-center h-100 px-md-5 bg-grey ">
+        <div class="container-fluid py-2 my-3 pr-lg-5 text-left bg-transparent ">
+            <div class="blockquote p-0 border bg-white">
                 <div class="d-flex">
-                    <div class="flex-column pl-5 pr-3 justify-content-center d-none d-md-flex ">
+                    <div class="flex-column px-3 pt-3 justify-content-start d-none d-md-flex ">
                         <img src="assets/david.jpg" class="rounded-img " alt="User Photo" />
                         <span class="text-center ">David Dinis</span>
                     </div>
-                    <div class="container-fluid bg-light border">
-                        <h2 class="font-weight-bolder pl-lg-2 ">
+                    <div class="container-fluid p-0 m-0">
+                        <h2 class="font-weight-bolder pt-3 pr-3">
                             How do I wash my yellow jacket?
-                            <a href="#" class="small text-secondary">
+                        </h2> <a href="#" class="small text-secondary">
                                 <i class="fa fa-flag "></i> Report</a>
-                        </h2>
-                        <p class="pl-lg-2 pt-lg-2">
+                        <p class="pt-2 pr-3">
                             SO, I have this yellow jacket and I don't know how to wash it. How do I wash
                             it? It is yellow. Please help.
                             <br />
                             EDIT: The jacket is yellow
                         </p>
-                        <span class="tag"> #Laundry</span> <br />
-                        <span class="text-left pt-lg-3">
-                            Asked on 16, June 2015
+                        <span class="tag tag-yellow">#Laundry</span> <br />
+                        <span class="text-left text-dark small pt-3">
+                            Asked on June 16, 2015
                         </span>
                     </div>
                 </div>
             </div>
             <!-- REPLY -->
-            <div class="blockquote py-lg-2 px-lg-5 d-flex flex-column">
-                <div class="d-flex  px-lg-5">
-                    <div class="d-flex flex-column px-4">
-                        <i class=" fa fa-angle-up fa-2x goodGreen"></i>
-                        <i class="">42</i>
-                        <i class=" fa fa-angle-down fa-2x badPurple "></i>
-                    </div>
-                    <div class="border d-flex p-3 w-100">
-                        <div class="d-flex flex-column align-items-center pr-4">
-                            <img src="assets/henrique.jpg" class="rounded-img" alt="User Photo" />
-                            <span class="text-center">Henrique Freitas</span>
+            <div class="blockquote m-0 p-0 d-flex flex-column ">
+                <div class="d-flex">
+                    <div class="px-3">
+                        <div class="rounded-img d-flex flex-column align-items-end h-100 justify-content-center ">
+                            <i class=" fa fa-angle-up fa-2x goodGreen"></i>
+                            <i class="">42</i>
+                            <i class=" fa fa-angle-down fa-2x badPurple "></i>
                         </div>
-                        <div>
-                            <span class="small">
-                                <span class="font-weight-bolder">4253 points </span>
-                                | Member since October 2010 | Posted 15 days ago |
-                                <a href="#" class="small text-secondary  text-nowrap">
-                                    <i class="fa fa-flag "></i>
-                                    Report
-                                </a>
-                                <span class="px-lg-5 font-weight-bold text-danger text-nowrap">
+                        
+                    </div>
+                    <div class="border d-flex flex-column w-100 p-0 bg-white">
+                            
+                            <span class="p-2 font-weight-bold text-danger text-nowrap">
                                     <i class="fa fa-heart "></i> Best Answer!</span>
-                            </span>
-                            <p class="pt-lg-3 pl-lg-2 ">
+                            <p class="px-2">
                                 In order to wash your yellow jacket you need to use special yellow jacket
                                 washing soap In order to wash your yellow jacket you need to use special
                                 yellow jacket washing soap In order to wash your yellow jacket you need to
                                 use special yellow jacket washing soap In order to wash your yellow jacket
                                 you need to use special yellow jacket
                             </p>
-                        </div>
+                            <div class="d-flex justify-content-between small px-2 pb-2 align-items-end">
+                                <span class=""> &#8212 Henrique Freitas <span class="small text-dark">| 4253 points 
+                                | Member since October 2010 | Replied on July 3, 2016</span></span>
+                                <a href="#" class="small text-secondary text-nowrap px-2">
+                                    <i class="fa fa-flag "></i>
+                                    Report
+                                </a>
+                                
+                            </div>
                     </div>
                 </div>
                 <!-- COMMENT -->
-                <div class="d-flex pt-2 justify-content-center px-lg-5">
-                    <div class="d-flex flex-column align-items-center pr-3">
-                        <img src="assets/rita.jpg" class="small rounded-img" alt="User Photo" />
-                    </div>
-                    <div class="border w-75">
-                        <span class=" small px-3">
-                            <span class="font-weight-bolder">3443 points</span>
-                            | Member since May 2015 | Posted 15 days ago |
-                            <a href="#" class="small text-secondary">
+                <div class="d-flex pt-2 justify-content-center">
+                    <div class="px-3"><div class="rounded-img h-25"></div></div>
+                    <div class="px-3"><div class="rounded-img h-25"></div></div>
+                    <div class="border w-100 bg-white ">
+                        
+                        
+                        <p class="py-2 px-2 small m-0">
+                            Indeed there is a special soap used for yellow jackets! Best answer for me!
+                        </p>
+                        <div class="d-flex justify-content-between small px-2 pb-2 align-items-end">
+                            <span > &#8212 Rita Mota <span class="small text-dark">| 3443 points
+                            | Member since May 2015 | Commented on September 24, 2019</span></span>
+                            <a href="#" class="small text-secondary text-nowrap px-2">
                                 <i class="fa fa-flag "></i>
                                 Report
                             </a>
-                        </span>
-                        <p class="py-lg-2 px-lg-2 small">
-                            Indeed there is a special soap used for yellow jackets! Best answer for me!
-                        </p>
+                        </div>
                     </div>
                 </div>
             </div>
--- A3_Base/html/tpl_common.php
+++ A3_Base/html/tpl_common.php
@@ -12,9 +12,9 @@ function drawHTMlHeader(){ ?>
     <link rel="stylesheet" href="css/customStyles.css">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:600&display=swap" rel="stylesheet">
     <script src="js/bootstrap.min.js" defer></script>
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico" />
     <title>GROW</title>
 </head>
 

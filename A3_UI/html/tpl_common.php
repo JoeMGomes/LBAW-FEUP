26cbc7b8d379d9b0f21124cb23a6b79273ac99ee
Jose Gomes <up201707054@fe.up.pt>
2020-03-17 20:35:29 +0000

    Started AdminPage

--- A3_UI/html/tpl_common.php
+++ A3_UI/html/tpl_common.php
@@ -26,7 +26,7 @@ function drawHTMlHeader()
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
         </script>
         <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-        <script src="js/scripts.js"></script>
+        <script src="js/scripts.js" defer></script>
 
 
     <?php }

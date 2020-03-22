8f39afd4ace443533a160be45b8f0a5904fb5b76
Jose Gomes <up201707054@fe.up.pt>
2020-03-22 16:19:07 +0000

    Updated Admin page

--- A3_UI/html/adminRepMan.php
+++ A3_UI/html/adminRepMan.php
@@ -2,6 +2,7 @@
 <?php
 
 include_once('tpl_common.php');
+include_once('tpl_admin.php');
 
 drawHTMlHeader();
 ?>
@@ -14,129 +15,34 @@ drawHTMlHeader();
     <main id="main" class="ml-lg-auto col-lg-10 align-right ">
         <h1 class=" py-2 px-4"> Administration</h1>
         <div class="container mx-5">
-            <h5>Pending Reports</h5>
-            <div class="table-responsive">
+            <?php drawAdminHead() ?>
 
+            <div class="table-responsive">
+                <h5>Pending Reports</h5>
                 <table class="table table-bordered table-striped fixed-header">
                     <thead class="">
                         <tr>
                             <th scope="col">ID</th>
                             <th scope="col">Offender</th>
                             <th scope="col">Date</th>
+                            <th scope="col">Offense</th>
                             <th scope="col">Post</th>
+                            <th scope="col">Description</th>
                             <th scope="col">Actions</th>
                         </tr>
                     </thead>
                     <tbody class="">
-                    <tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr><tr>
-                            <td
-                            <td>12333</td>
-                            <td>Rita Mota</td>
-                            <td>26/05/18</td>
-                            <td>ARE YOU RETARDED??</td>
-                            <td><i class="fa fa-check text-success" aria-hidden="true">
-                                <i class="fas fa-ban text-danger "></i>
-                            </i></td>
-                        </tr>
+                        <?php
+                        drawTableEntry();
+                        drawTableEntry();
+                        drawTableEntry();
+                        drawTableEntry();
+                        drawTableEntry();
+                        drawTableEntry();
+                        drawTableEntry();
+                        drawTableEntry();
+                        drawTableEntry();
+                        ?>
                     </tbody>
                 </table>
 
@@ -145,3 +51,23 @@ drawHTMlHeader();
     </main>
 
 </body>
+<?php
+
+function drawTableEntry()
+{
+?>
+
+    <tr>
+        <td class="align-middle text-center">12333</>
+        <td class="align-middle text-center">Rita Mota</td>
+        <td class="align-middle text-center">26/05/18</td>
+        <td class="align-middle text-center">Offensive Towards Others</td>
+        <td class="align-middle text-center "><a class="text-black" href="#">ARE YOU RETARDED??</a></td>
+        <td class="align-middle text-center">The answer is very rude and has no actual asnwer to the question asked</td>
+        <td class="align-middle text-center ">
+            <button class="btn text-danger">Delete</button>
+            <button class="btn text-success">Keep</i></button>
+        </td>
+    </tr>
+
+<?php } ?>
---
+++ A3_UI/html/tpl_admin.php
@@ -0,0 +1,9 @@
+<?php
+function drawAdminHead()
+{
+?> <div class="my-2 mb-4">
+        <h4><a href="adminRepMan.php" class="text-black large d-inline-block">Report Management</a> |
+            <a href="adminCatMan.php" class="text-black d-inline-block">Category Management</a>
+        </h4>
+    </div>
+<?php } ?>

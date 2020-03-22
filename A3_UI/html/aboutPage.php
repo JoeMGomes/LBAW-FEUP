ae7be92ce680edeefc300a65dc5081458c069161
Jose Gomes <up201707054@fe.up.pt>
2020-03-22 23:54:12 +0000

    Added About page

---
+++ A3_UI/html/aboutPage.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once('tpl_common.php');
+
+drawHTMLHeader()
+
+?>
+
+<body style="background-image: url('assets/pattern.png');">
+    <?php drawNavBar(false);
+    drawMenuBtnMainPage(); ?>
+
+    <main id="main" class="ml-sm-auto col-lg-10 px-4 d-flex flex-column align-items-center justify-content-between">
+        <div class="d-flex flex-column m-md-5 m-sm-1 border bg-white p-md-5 p-sm-1 mw-75 mt-4 pt-4 px-2">
+            <h1>About us</h1>
+            <p>We are a group of students that made a website because our teachers told us to. 
+                Don't hate the player, hate the game.
+                But since you're here, take a look at some posts and maybe Grow a little &semi;&#41; 
+            </p>
+            <div class="mw-50 mx-auto">
+                <div class="d-flex justify-content-between align-items-center my-4">
+                    <img src="assets/henriqueF.jpg" class="rounded-img " alt="User Photo" />
+                    <label class="mx-3">Henrique Freitas</label>
+                </div>
+                <div class="d-flex justify-content-between align-items-center my-4">
+                    <label class="mx-3">Rita Mota</label>
+                    <img src="assets/rita.jpg" class="rounded-img " alt="User Photo" />
+                </div>
+                <div class="d-flex justify-content-between align-items-center my-4">
+                    <img src="assets/joseG.jpg" class="rounded-img " alt="User Photo" />
+                    <label class="mx-3">José Gomes</label>
+                </div>
+                <div class="d-flex justify-content-between align-items-center my-4">
+                    <label class="mx-3">David Dinis</label>
+                    <img src="assets/david.jpg" class="rounded-img " alt="User Photo" />
+                </div>
+            </div>
+            <p class="align-self-center pt-2">Laboratório de Bases de Dados e Aplicações Web, 2019/20</p>
+        </div>
+    </main>
+</body>
+
+</html>
--- A3_UI/html/activityPage.php
+++ A3_UI/html/activityPage.php
@@ -9,36 +9,33 @@ drawHTMLHeader()
 
 <body class=" vh-100 m-0 p-0">
     <!-- <nav class="position-fixed col-md-2 col-lg-2 d-none d-md-flex flex-column  justify-content-between  bg-dark sidebar h-100"> -->
-        <?php 
-            drawNavBar(true);
-            drawNavBarTop("");
-        ?>
-       
+    <?php
+    drawNavBar(true);
+    drawNavBarTop("");
+    ?>
+
 
 
     <main id="main" class="ml-lg-auto col-lg-10  ">
         <h2 class="p-4">Activity Page</h2>
         <div class="container-fluid py-0 my-0 pr-lg-5 text-left bg-white ">
-            <p class="h6"> <i class="fa fa-calendar fa-1x"> </i> 21/03/2020</p>
-            <div class="blockquote py-2 d-flex p-3 bg-mygrey rounded">
+            <p class="h6 my-3"> <i class="fa fa-calendar fa-1x"> </i> 21/03/2020</p>
+            <div class="border d-flex flex-column bg-light mb-3">
                 <div class="d-flex m-0 ">
-                    <div class="row px-3 py-1">
+                    <div class="row w-100 px-3 ml-2 py-1">
                         <div class="d-flex w-100">
                             <span>You asked a question!</span>
                             <i class="fa fa-pencil px-2"></i>
                             <a href="#">Edit question</a>
                         </div>
-
-                        <div>
-
+                        <div class="my-2">
                             <h5 class=" pt-2">
                                 Do I need to keep all my receipts from the past year?
                             </h5>
                             <span> taxes are hard, how does it work??</span>
                         </div>
-                        <div class="rounded d-flex w-100 bg-white ">
+                        <div class="d-flex w-100 border bg-white mx-4 ">
                             <div class="pt-2 ">
-
                                 <span class="px-3  text-danger text-nowrap">
                                     <i class="fa fa-heart "></i>
                                     Best Answer!</span>
@@ -46,50 +43,47 @@ drawHTMLHeader()
                                     You can take things and not pay if you want
                                 </p>
                             </div>
-
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="blockquote py-2 rounded d-flex p-3 bg-mygrey">
-                <div class="d-flex m-0 flex-column w-100">
-                    <div class="d-flex w-100">
-                        <span class="px-2">You helped someone and gained 23 points!</span>
-                        <i class="fa fa-pencil px-2"></i>
-                        <a href="#">Edit answer</a>
-                    </div>
-                    <div class="rounded d-flex w-100 bg-white">
-                        <div class=" d-flex">
+            <div class="border d-flex flex-column bg-light mb-3">
+                <div class="d-flex m-0">
+                    <div class="row w-100 px-3 ml-2 py-1">
+                        <div class="d-flex w-100">
+                            <span class="px-2">You helped someone and gained<b> 23</b> points!</span>
+                            <i class="fa fa-pencil px-2"></i>
+                            <a href="#">Edit answer</a>
+                        </div>
+                        <div class=" d-flex w-100 border bg-white mx-4 ">
                             <div class="d-flex flex-column px-4">
                                 <i class=" fa fa-angle-up fa-2x text-mydark"></i>
                                 <i>23</i>
                                 <i class=" fa fa-angle-down fa-2x text-mydark "></i>
                             </div>
-                            <p class="pt-3 pl-2">
+                            <p class="pt-3 pl-2 ">
                                 Download more RAM?
                             </p>
                         </div>
-
                     </div>
-
                 </div>
             </div>
-            <p class="h6"> <i class="fa fa-calendar fa-1x"> </i> 15/03/2020</p>
-            <div class="blockquote py-2 d-flex p-3 bg-mygrey rounded">
+            <p class="h6 my-3"> <i class="fa fa-calendar fa-1x"> </i> 15/03/2020</p>
+            <div class="border d-flex flex-column bg-light mb-3">
                 <div class="d-flex m-0 ">
-                    <div class="row px-3 py-1">
+                    <div class="row px-3 ml-2 py-1 w-100">
                         <div class="d-flex w-100">
                             <span>You commented in a reply!</span>
                             <i class="fa fa-pencil px-2"></i>
                             <a href="#">Edit comment</a>
                         </div>
-                        <div>
+                        <div class="my-2">
                             <h5 class="pt-2">
                                 How do I wash my yellow jacket?
                             </h5>
                             <span> My jacket is yellow and is dirty</span>
                         </div>
-                        <div class="rounded d-flex w-100 bg-white ">
+                        <div class="d-flex w-100 border bg-white mx-4">
                             <div class="pt-2 ">
                                 <p class="pt-2 px-3">
                                     Indeed there is a special soap used for yellow jackets! Best answer for me!

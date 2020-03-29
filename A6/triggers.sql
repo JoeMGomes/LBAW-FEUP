f47c1070ddf9564605a0ef575d7ba2de1ff20a0f
david <up201706766@fe.up.pt>
2020-03-29 18:28:17 +0100

    self vote and self report

--- A6/triggers.sql
+++ A6/triggers.sql
@@ -34,7 +34,6 @@ DROP TRIGGER IF EXISTS "ban" ON "report";
 CREATE TRIGGER ban AFTER UPDATE ON "report"
     FOR EACH ROW EXECUTE PROCEDURE ban_member();
 	
-
 --
 	
 CREATE OR REPLACE FUNCTION categories()
@@ -57,10 +56,9 @@ DROP TRIGGER IF EXISTS "check_max_categories" ON "question_category";
 CREATE TRIGGER check_max_categories BEFORE INSERT on question_category
 	FOR EACH ROW EXECUTE PROCEDURE categories();
 
-
 --
 
-CREATE OR REPLACE notification_comment()
+CREATE OR REPLACE FUNCTION notification_comment()
 	RETURNS trigger AS 
 $$
 DECLARE notification_id notification.id%TYPE;
@@ -69,16 +67,16 @@ BEGIN
 	SELECT INTO author_post author FROM post WHERE NEW.post = post.id;
  	INSERT INTO notification(notified) VALUES (author_post) RETURNING id INTO notification_id;
 	INSERT INTO post_notif VALUES (notification_id, NEW.post);
+	RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
 DROP TRIGGER IF EXISTS comment_notification on "comment";
 CREATE TRIGGER comment_notification AFTER INSERT on "comment"
 	FOR EACH ROW EXECUTE PROCEDURE notification_comment();
 
-
 --
 
-CREATE OR REPLACE notification_answer()
+CREATE OR REPLACE FUNCTION notification_answer()
 	RETURNS trigger AS 
 $$
 DECLARE notification_id notification.id%TYPE;
@@ -87,17 +85,16 @@ BEGIN
 	SELECT INTO author_post author FROM post WHERE NEW.post = post.id;
  	INSERT INTO notification(notified) VALUES (author_post) RETURNING id INTO notification_id;
 	INSERT INTO post_notif VALUES (notification_id, NEW.post);
+	RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
 DROP TRIGGER IF EXISTS answer_notification on "answer";
 CREATE TRIGGER answer_notification AFTER INSERT on "answer"
 	FOR EACH ROW EXECUTE PROCEDURE notification_answer();
 
-
-
 --
 
-CREATE OR REPLACE notification_vote()
+CREATE OR REPLACE FUNCTION notification_vote()
 	RETURNS trigger AS 
 $$
 DECLARE notification_id notification.id%TYPE;
@@ -106,11 +103,64 @@ BEGIN
 	SELECT INTO author_post author FROM post, answer WHERE (NEW.post = answer.id AND answer.post = post.id);
  	INSERT INTO notification(notified) VALUES (author_post) RETURNING id INTO notification_id;
 	INSERT INTO vote_notif VALUES (notification_id, NEW.post);
+	RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
 DROP TRIGGER IF EXISTS vote_notification on "vote";
 CREATE TRIGGER vote_notification AFTER INSERT on "vote"
 	FOR EACH ROW EXECUTE PROCEDURE notification_vote();
+
+--
+
+CREATE OR REPLACE FUNCTION log_edit()
+	RETURNS trigger AS
+$$
+BEGIN
+	INSERT INTO edit_log(post, old_body) VALUES (OLD.id, OLD.text_body);
+RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+DROP TRIGGER IF EXISTS edit_log on post;
+CREATE TRIGGER edit_log AFTER UPDATE on post
+	FOR EACH ROW EXECUTE PROCEDURE log_edit();
+
+--
+
+CREATE OR REPLACE FUNCTION self_vote()
+	RETURN trigger AS
+$$
+DECLARE author_post post.author%TYPE;
+BEGIN
+	SELECT INTO author_post author FROM post, answer WHERE (NEW.post = answer.id AND answer.post = post.id);
+	IF NEW.voter = author_post THEN
+		RAISE EXCEPTION 'A member cannot upvote itself';
+		RETURN NULL;
+	END IF;
+RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+DROP TRIGGER IF EXISTS vote_self on vote;
+CREATE TRIGGER vote_self BEFORE INSERT on vote
+	FOR EACH ROW EXECUTE PROCEDURE self_vote();
+
+--
+
+CREATE OR REPLACE FUNCTION self_report()
+	RETURN trigger AS
+$$
+DECLARE author_post post.author%TYPE;
+BEGIN
+	SELECT INTO author_post author FROM post WHERE NEW.reported = post.id;
+	IF NEW.reporter = author_post THEN
+		RAISE EXCEPTION 'A member cannot report itself';
+		RETURN NULL;
+	END IF;
+RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+DROP TRIGGER IF EXISTS report_self on report;
+CREATE TRIGGER report_self BEFORE INSERT on report
+	FOR EACH ROW EXECUTE PROCEDURE self_report();
 
 
 

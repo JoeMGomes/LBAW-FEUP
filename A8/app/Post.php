8b3e527c810cd8e9fe743581babd4274158eae7a
david <up201706766@fe.up.pt>
2020-04-16 18:26:43 +0100

    question model

--- A8/app/Post.php
+++ A8/app/Post.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'post';
+
+
+    protected $fillable = [
+        'author', 'date', 'text_body'
+    ];
+
+    public function author() {
+        return $this->belongsTo('App\Member', 'author');
+    }
+
+
 }

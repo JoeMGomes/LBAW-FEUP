38f52d2e5efad462f489266ccf46a8c15ed642b3
Rita <up201703964@fe.up.pt>
2020-05-30 22:42:19 +0100

    post comment

--- A8/routes/web.php
+++ A8/routes/web.php
@@ -40,6 +40,7 @@ Route::post('api/downvote', 'VoteController@downvote');
 Route::post('api/bestAnswer', 'QuestionController@chooseBestAnswer');
 Route::post('api/category', 'CategoryController@getCategories');
 Route::post('/deleteQuestion', 'QuestionController@deleteQuestion');
+Route::post('post/addComment', 'CommentController@addComment')->name('addComment');
 
 // Module 03
 Route::get('/', 'HomeController@showHome')->name('home');

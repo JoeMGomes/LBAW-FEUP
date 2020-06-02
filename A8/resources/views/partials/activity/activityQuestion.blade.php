9f1395db04d908e1394216cae00b7ffc56fc4fb3
Rita <up201703964@fe.up.pt>
2020-06-02 15:57:00 +0100

    Activity Page finished

--- A8/resources/views/partials/activity/activityQuestion.blade.php
+++ A8/resources/views/partials/activity/activityQuestion.blade.php
@@ -1,20 +1,20 @@
-<p class="h6 my-3"> <i class="fa fa-calendar fa-1x"> </i>  {{date('M d, Y', strtotime($post['date']))}}</p>
-<div class="border d-flex flex-column bg-light mb-3">
-    <div class="d-flex m-0 ">
-        <div class="row w-100 px-3 ml-2 py-1">
+<p class="h6 my-3"> <i class="fa fa-calendar fa-1x mr-2"> </i>  {{date('M d, Y', strtotime($post['date']))}}</p>
+<div class="border w-100 bg-light">
+    <div class="d-flex w-100">
+        <div class="row w-100 px-3 ml-2 py-1 flex-column">
             <div class="d-flex w-100 align-items-center">
                 <span>You asked a question!</span>
                 @include('partials.question.deleteQuestion',  ['question' => $post])
             </div>
-            <div class="mb-1">
-                <h5 class=" pt-2">
+            <div class="mb-3">
+                <h5 class=" ">
                     <a class="text-black" href="{{ url('post/'.$post['id']) }}">
                     {{$post['title']}}
                     </a>
                 </h5>
                 <span> {{$post['text']}}</span>
             </div>
-            <div class="d-flex w-100 border bg-white mx-4 ">
+            <div class="d-flex w-50 border bg-white mx-4 ">
                 <div class="pt-2 ">
                     <span class="px-3  text-danger text-nowrap">
                         <i class="fa fa-heart "></i>

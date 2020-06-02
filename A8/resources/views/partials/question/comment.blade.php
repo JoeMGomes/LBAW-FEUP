44b5e61ab5f7310348089d02b20febf1ad6a5ad5
Rita <up201703964@fe.up.pt>
2020-06-02 17:21:33 +0100

    small changes

--- A8/resources/views/partials/question/comment.blade.php
+++ A8/resources/views/partials/question/comment.blade.php
@@ -1,12 +1,12 @@
 <div class="d-flex align-items-center mt-1 w-100">
     <div class="rounded-img  d-none d-lg-block"> </div>
     <div class="rounded-img"> </div>
-    <div class="border w-100 d-flex flex-column mx-lg-4 px-3 py-3">
+    <div class="border w-100 d-flex flex-column mx-lg-4 px-3 pt-3">
         <p class="text-justify">
         @if ($comment['edited'])<small>(<u>Edited</u>)</small>@endif{!! Illuminate\Mail\Markdown::parse(nl2br(e($comment['text']))) !!}
         </p>
         <div class="d-flex flex-wrap justify-content-between align-items-center">
-            <div class="flex-column align-items-center mr-3">
+            <div class="flex-column align-items-center mt-0 mr-3">
                 <span class="text-nowrap">
                     <img src="{{asset('img/'.$comment['photo_url'])}}" class="comment-img mr-2" alt="User Photo" />
                     &#8212 {{$comment['name']}}

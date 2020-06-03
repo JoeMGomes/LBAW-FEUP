4f330f3be10355717faa3bb181726859a38fc402
david <up201706766@fe.up.pt>
2020-06-03 16:45:36 +0100

    change db acess to model only

--- A8/app/Http/Controllers/QuestionController.php
+++ A8/app/Http/Controllers/QuestionController.php
@@ -60,17 +60,13 @@ class QuestionController extends Controller
         }
             $this->authorize('create', Question::class);
 
-            $id = DB::select('SELECT add_question(:param1, :param2, :param3)', [
-            'param1' => Auth::user()->id, 
-            'param2' => $textBody, 
-            'param3' => $title]);
+            $obj = new Question();
+            $id = $obj->addQuestion($title, $textBody);
             $id = collect($id)->map(function($x) {return (array) $x; })->toArray();
             $questionid = $id[0]['add_question'];
             
             for ($i = 0; $i < count($catArray); $i++){
-                DB::select('insert into question_category(question, category) values (:question, (select id from category where name = :catid));', [
-                    'question' => $questionid,
-                    'catid'  => $catArray[$i]]);
+                $obj->addCategory($questionid, $catArray[$i]);
             }
             return redirect('/post/'.$questionid);
     }
@@ -109,24 +105,19 @@ class QuestionController extends Controller
     }
 
     public function addAnswer(Request $request){
-        DB::select('SELECT add_answer(:param1, :param2, :param3)', [
-            'param1' => Auth::user()->id, 
-            'param2' => $request->input('text_body'), 
-            'param3' => $request->input('question_id')]);
+        $obj = new Question();
+        $obj->addAnswer($request);
             return redirect('/post/'. $request->input('question_id'));
     }
 
     public function chooseBestAnswer(Request $request) {
-        $question = $request->input('question'); 
-        $answer = $request->input('answer');
-        DB::select(DB::raw('update question set best_answer = :answer where post = :question'),
-        ['question' => $question, 'answer' => $answer]);
+        $obj = new Question();
+        $obj->addbestAnswer($request);
     }
 
     public function deleteQuestion(Request $r) {
-        $question = $r->input('id');
-        DB::select(DB::raw('select delete_question(:question)'), 
-            ['question' => $question]);
+        $obj = new Question();
+        $obj->delQuestion($r);
         return redirect('/');
     }
 

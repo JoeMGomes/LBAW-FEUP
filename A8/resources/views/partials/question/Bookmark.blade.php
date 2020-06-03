43f289d2df8b72dbcb4fcf99d47a89daee6e16c4
henrique <up201707046@fe.up.pt>
2020-06-03 13:45:10 +0100

    votes refactored

--- A8/resources/views/partials/question/Bookmark.blade.php
+++ A8/resources/views/partials/question/Bookmark.blade.php
@@ -3,25 +3,25 @@
 @if($question['bookmarked'])
 <div  id="bookmarked">
     <button type="button" class="btn text-secondary" onclick="deleteBookmark({{$question['id']}})">
-        <a href="#" class=" text-black"><i class="fa fa-bookmark"></i> bookmarked!</a>
+        <a href="#" class=" text-black"><i class="fa fa-bookmark"></i> Bookmarked!</a>
     </button>
 </div>
 
 <div class="d-none" id="bookmark">
     <button type="button" class="btn text-secondary" onclick="addBookmark({{$question['id']}})">
-        <a href="#" class=" text-black"><i class="fa fa-bookmark-o"></i> bookmark</a>
+        <a href="#" class=" text-black"><i class="fa fa-bookmark-o"></i> Bookmark</a>
     </button>
 </div>
 @else
 <div  class="d-none" id="bookmarked">
     <button  type="button" class="btn text-secondary" onclick="deleteBookmark({{$question['id']}})">
-        <a href="#" class=" text-black"><i class="fa fa-bookmark"></i> bookmarked!</a>
+        <a href="#" class=" text-black"><i class="fa fa-bookmark"></i> Bookmarked!</a>
     </button>
 </div>
 
 <div id="bookmark">
     <button type="button" class="btn text-secondary" onclick="addBookmark({{$question['id']}})">
-        <a href="#" class=" text-black"><i class="fa fa-bookmark-o"></i> bookmark</a>
+        <a href="#" class=" text-black"><i class="fa fa-bookmark-o"></i> Bookmark</a>
     </button>
 </div>
 @endif
